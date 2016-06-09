45950bac679661c582eb3db1cb6e2c44e662e7e0
xiaoyanhao <270411549@qq.com>
2016-06-09 16:30:34 +0800

    ignore comments in html; rename

--- index.js
+++ index.js
@@ -7,37 +7,41 @@ var fs = require('fs');
 
 // Consts
 var PLUGIN_NAME = 'gulp-bower-files-from-html';
-var script = /<script([^>]*)>\s*<\/script>/gi;
-var src = /src=['"]([^"']*)['"]/i;
-var bower = /bower_components/;
-var bower_dir = null;
+var SCRIPT = /<script([^>]*)>\s*<\/script>/gi;
+var SRC = /src=['"]([^"']*)['"]/i;
+var COMMENT = /<!--[^]*?-->/g;
+var BOWER = /bower_components/;
+var BOWER_PATH = null;
 
-// find out the absolute path of bower
-function dealWithBowerPath(bower_path) {
+// get the absolute path of bower components
+function getAbsolutePath(path) {
   var dir = 'bower_components';
 
-  if (bower_path) {
-    var len = bower_path.length;
-    if (bower_path[len - 1] == '/') {
-      dir = bower_path.slice(0, -1);
+  if (path) {
+    var len = path.length;
+    if (path[len - 1] == '/') {
+      dir = path.slice(0, -1);
     } else {
-      dir = bower_path;
+      dir = path;
     }
   }
 
   return __dirname + '/' + dir;
 }
 
+// deal with html file buffer
 function dealWithFileBuffer(file, self) {
   var html = file.contents.toString();
-  var scripts = html.match(script);
+  // just ignore comments in html
+  html = html.replace(COMMENT, '');
+  var scripts = html.match(SCRIPT);
   var paths = [];
 
   scripts.forEach(function(script) {
-    var path = script.match(src);
-    if (path != null && bower.test(path[1])) {
-      path = path[1].split(bower)[1];
-      path = bower_dir + path;
+    var path = script.match(SRC);
+    if (path != null && BOWER.test(path[1])) {
+      path = path[1].split(BOWER)[1];
+      path = BOWER_PATH + path;
       paths.push(path);
     }
   });
@@ -60,7 +64,7 @@ function dealWithFileBuffer(file, self) {
 
 // Plugin level function(dealing with files)
 function gulpBowerFilesFromHtml(bower_path) {
-  bower_dir = dealWithBowerPath(bower_path);
+  BOWER_PATH = getAbsolutePath(bower_path);
 
   // Creating a stream through which each file will pass
   var stream = through.obj(function(file, enc, cb) {
@@ -72,15 +76,16 @@ function gulpBowerFilesFromHtml(bower_path) {
     if (file.isStream()) {
       var self = this;
       file.contents.on('data', function(chunck) {
+        // stream to buffer
         file.contents = chunck;
         dealWithFileBuffer(file, self);
+        // tell the stream engine that we are done with this file
         cb();
       });
     }
 
     if (file.isBuffer()) {
       dealWithFileBuffer(file, this);
-      // tell the stream engine that we are done with this file
       cb();
     }
   });
